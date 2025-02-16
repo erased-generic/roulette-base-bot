768b600def82d493fa55ef62d14ff9fa6b98aefd
erased-generic <284558@niuitmo.ru>
2025-02-17 02:15:48 +0300

    Fix description

--- src/bot/anagramsduelimpl.ts
+++ src/bot/anagramsduelimpl.ts
@@ -11,8 +11,13 @@ class AnagramsDuelImpl extends DuelImpl<anagramsModule.Anagrams> {
   readonly handlers: { [key: string]: DuelHandler } = {
     hint: {
       action: this.getHint.bind(this),
-      description: "Get a hint for a random word",
-      format: "[<word>]",
+      description: "Get a hint for a specific word or a random word if none was specified",
+      format: "[<the word>]",
+    },
+    Hint: {
+      action: this.testCaseSensitive.bind(this),
+      description: "Test command case-sensitivity",
+      format: "",
     }
   };
   readonly bindMoves: { [key: string]: DuelMove } = {
@@ -114,5 +119,10 @@ class AnagramsDuelImpl extends DuelImpl<anagramsModule.Anagrams> {
       return `Hint: an answer for ${hint.word} looks like ${hint.hint}!`;
     }
     return "No duel - no hint!";
+  }
+
+  testCaseSensitive(bot: DuelBot, context: ChatContext, args: string[]): string | undefined {
+    console.log(`* testCaseSensitive context: ${JSON.stringify(context)}, args: ${JSON.stringify(args)}`);
+    return `context: ${JSON.stringify(context)}, args: ${JSON.stringify(args)}`;
   }
 }
